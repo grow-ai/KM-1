96ab0f017e6768dff1ec9727f884fe76a37e3416
brianfu9 <brianfu9@gmail.com>
2021-04-09 18:19:30 -0700

    camera 1 works

--- platforms/electron/www/index.js
+++ platforms/electron/www/index.js
@@ -3,8 +3,6 @@ var app = {
     initialize: function () {
         console.log('start');
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        // Disabling form submissions if there are invalid fields
-        this.disableFormSubmission();
     },
 
     // deviceready Event Handler
@@ -18,25 +16,6 @@ var app = {
         } else {
             alert('getUserMedia() is not supported by your browser :/');
         }
-    },
-
-    disableFormSubmission: function () {
-        window.addEventListener('load', function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            const forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    } else {
-                        alert('Form submitted!');
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
     },
 
     hasGetUserMedia: function () {
